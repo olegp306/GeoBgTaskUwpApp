740d53074743b65f2fbb4433fc96de80d39cec0b
elpy <silmion@gmail.com>
2016-12-29 17:00:13 +0600

    Инит таск

--- GeoBgTaskUwpApp/MainPage.xaml.cs
+++ GeoBgTaskUwpApp/MainPage.xaml.cs
@@ -6,9 +6,15 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Networking;
+using Windows.Networking.Sockets;
 using Windows.Security.Credentials;
+using Windows.Storage.Streams;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -27,13 +33,67 @@ namespace GeoBgTaskUwpApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private const string socketId = "GeoSocket";
+        private StreamSocket socket = null;
+        private IBackgroundTaskRegistration task = null;
+        private const string port = "22668";
+
+
         public MainPage()
         {
             this.InitializeComponent();
         }
 
-        private void UserName_GotFocus(object sender, RoutedEventArgs e)
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
         {
+            base.OnNavigatedTo(e);
+
+            Geolocator locator = new Geolocator();
+            var accessStatus = await Geolocator.RequestAccessAsync();
+            LoadMe();
+
+            try
+            {
+                foreach (var current in BackgroundTaskRegistration.AllTasks)
+                {
+                    if (current.Value.Name == "BackgroundTask")
+                    {
+                        task = current.Value;
+                        break;
+                    }
+                }
+
+                // If there is no task allready created, create a new one
+                if (task == null)
+                {
+                    var socketTaskBuilder = new BackgroundTaskBuilder();
+                    socketTaskBuilder.Name = "BackgroundTask";
+                    socketTaskBuilder.TaskEntryPoint = "BackgroundTask.SocketActivityTask";
+                    var trigger = new SocketActivityTrigger();
+                    socketTaskBuilder.SetTrigger(trigger);
+                    task = socketTaskBuilder.Register();
+                }
+
+                SocketActivityInformation socketInformation;
+                if (SocketActivityInformation.AllSockets.TryGetValue(socketId, out socketInformation))
+                {
+                    // Application can take ownership of the socket and make any socket operation
+                    // For sample it is just transfering it back.
+                    socket = socketInformation.StreamSocket;
+                    socket.TransferOwnership(socketId);
+                    socket = null;
+
+                    LogListView.Items.Add("Connected. You may close the application");
+                    /*rootPage.NotifyUser("Connected. You may close the application", NotifyType.StatusMessage);
+                    TargetServerTextBox.IsEnabled = false;
+                    ConnectButton.IsEnabled = false;*/
+                }
+
+            }
+            catch (Exception exception)
+            {
+                await new MessageDialog(exception.Message).ShowAsync();
+            }
 
         }
 
@@ -126,8 +186,8 @@ namespace GeoBgTaskUwpApp
                 //    (App.Current as App).GeoSynchronizationTask.Start();
 
 
-               // Jump();
-
+                // Jump();
+                await InitTask();
             }
             catch (HttpRequestException e0)
             {
@@ -160,6 +220,7 @@ namespace GeoBgTaskUwpApp
                 SetStateOfControls(true);
             }
         }
+
         /// <summary>
         /// Сохраняет учетную запись пользователя в хранилище
         /// </summary>
@@ -178,11 +239,69 @@ namespace GeoBgTaskUwpApp
         }
 
         /// <summary>
+        /// Загружает учетную запись пользователя из хранилища
+        /// </summary>
+        private void LoadMe()
+        {
+            var vault = new PasswordVault();
+            try
+            {
+                var crs = vault.FindAllByResource("geoclaris");
+                if (crs.Any())
+                {
+                    var cr = crs.First();
+                    UserName.Text = cr.UserName;
+                    cr.RetrievePassword();
+                    Password.Password = cr.Password;
+                }
+            }
+            catch { }
+        }
+
+
+        /// <summary>
         /// Настраивает доступность контролов
         /// </summary>
         private void SetStateOfControls(bool isEnabled)
         {
             UserName.IsEnabled = Password.IsEnabled = Login.IsEnabled = isEnabled;
+        }
+
+
+
+
+        /// <summary>
+        /// Инициализация фоновой задачи
+        /// </summary>
+        private async Task InitTask()
+        {
+            try
+            {
+                SocketActivityInformation socketInformation;
+                if (!SocketActivityInformation.AllSockets.TryGetValue(socketId, out socketInformation))
+                {
+                    socket = new StreamSocket();
+                    socket.EnableTransferOwnership(task.TaskId, SocketActivityConnectedStandbyAction.Wake);
+                    var targetServer = new HostName("192.168.0.43");
+                    await socket.ConnectAsync(targetServer, port);
+                    // To demonstrate usage of CancelIOAsync async, have a pending read on the socket and call 
+                    // cancel before transfering the socket. 
+                    /*DataReader reader = new DataReader(socket.InputStream);
+                    reader.InputStreamOptions = InputStreamOptions.Partial;
+                    var read = reader.LoadAsync(250);
+                    read.Completed += (info, status) =>
+                    {
+
+                    };
+                    await socket.CancelIOAsync();*/
+                    socket.TransferOwnership(socketId);
+                    socket = null;
+                }
+            }
+            catch (Exception exception)
+            {
+                await new MessageDialog(exception.Message).ShowAsync();
+            }
         }
     }
 }
